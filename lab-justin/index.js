82ddbfee9b2d8ff0abf5c9ab4ff3e05057ec489c
Justin Morris <justinjodymorris@gmail.com>
2018-05-17 10:59:55 -0700

    after code Review

---
+++ lab-justin/index.js
@@ -0,0 +1,10 @@
+'use strict'; 
+
+const fileReader = require('./lib/reader.js');
+
+let path = [`${__dirname}/data/file1.txt`];
+
+fileReader(path, (err, data) => {
+  console.log(err);
+});
+// fileReader()
--- lab-justin/lib/reader.js
+++ lab-justin/lib/reader.js
@@ -1,17 +1,18 @@
 'use strict';
 
+//TODO: change type to not be typeof object and use instanceof Array
+
 const fs = require('fs');
 
 module.exports = exports = (path, callback) => {
   let type = typeof path;
   let dataArray = [];
 
-  if (type === 'object' && path.length) {
+  if (type === 'object' && path.length) { // Don't use type of Object for an Array
     for (let i = 0; i < path.length; i ++ ) {
       fs.readFile(path[i], (error, data) => {
         if (error) {
           callback(error);
-    
         }
         else {
           dataArray.push(data.toString().trim());
@@ -22,7 +23,7 @@ module.exports = exports = (path, callback) => {
   } else {
     fs.readFile(path, (err, data) => {
       if (err) { callback(err); }
-      else { callback(undefined, data.toString().trim()); }
+      else { callback(data.toString().trim()); }
     });
   }
 };
