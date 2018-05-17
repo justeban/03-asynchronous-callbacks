2d11087e6895c1af6112985f6be5c8ba4c6f41ce
Justin Morris <justinjodymorris@gmail.com>
2018-05-16 22:27:12 -0700

    created module that will handle arrays and single files

--- lab-justin/lib/reader.js
+++ lab-justin/lib/reader.js
@@ -3,8 +3,26 @@
 const fs = require('fs');
 
 module.exports = exports = (path, callback) => {
-  fs.readFile(path, (err, data) => {
-    if (err) { callback(err); }
-    else { callback(undefined, data.toString().trim()); }
-  });
+  let type = typeof path;
+  let dataArray = [];
+
+  if (type === 'object' && path.length) {
+    for (let i = 0; i < path.length; i ++ ) {
+      fs.readFile(path[i], (error, data) => {
+        if (error) {
+          callback(error);
+    
+        }
+        else {
+          dataArray.push(data.toString().trim());
+        }
+      });
+    }
+    callback(null, dataArray);
+  } else {
+    fs.readFile(path, (err, data) => {
+      if (err) { callback(err); }
+      else { callback(undefined, data.toString().trim()); }
+    });
+  }
 };
